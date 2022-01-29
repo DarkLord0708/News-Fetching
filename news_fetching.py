157bfb66615ce1b1143fb9c05a7f5b21812cc762
Shivansh Yadav <71645247+Shivansh0708@users.noreply.github.com>
2022-01-29 11:50:01 +0530

    removed my api

--- news_fetching.py
+++ news_fetching.py
@@ -7,7 +7,7 @@ import json
 #     speak.Speak(str)
 
 if __name__ == '__main__':
-    url = "https://newsapi.org/v2/everything?q=tesla&from=2021-12-06&sortBy=publishedAt&apiKey=ff3ad897d91446ddb484113af3c33571"
+    url = "paste your api url here"
     news = requests.get(url).text
     news_dict = json.loads(news)
     arts = news_dict['articles']
